aeb3a33f4e856c380605f72740ee4a6693c011dc
Trac Lam <trac.lam@elinext.com>
2026-05-26 17:53:08 +0700

    Fix lottery parser prize labels for Telegram win messages

--- app/src/Services/LotteryParser.php
+++ app/src/Services/LotteryParser.php
@@ -9,23 +9,62 @@ class LotteryParser
     ): array {
 
         preg_match_all(
-            '/<td class="(giai[a-z0-9]+)">\s*(.*?)\s*<\/td>/s',
+            '/<td\b[^>]*class="(giai[a-z0-9]+)"[^>]*>\s*(.*?)\s*<\/td>/si',
             $js,
             $matches,
             PREG_SET_ORDER
         );
 
+        $labels = [];
+
+        foreach ($matches as $m) {
+
+            $class = strtolower(trim($m[1]));
+
+            if (!str_ends_with($class, 'l')) {
+
+                continue;
+            }
+
+            $baseClass = substr($class, 0, -1);
+
+            $label = trim(
+                strip_tags(
+                    html_entity_decode($m[2])
+                )
+            );
+
+            if ($baseClass !== '' && $label !== '') {
+
+                $labels[$baseClass] = $label;
+            }
+        }
+
         $results = [];
 
         foreach ($matches as $m) {
 
-            $name = strip_tags($m[1]);
+            $class = strtolower(trim($m[1]));
+
+            if (str_ends_with($class, 'l')) {
+
+                continue;
+            }
 
             $value = trim(
                 strip_tags(
                     html_entity_decode($m[2])
                 )
             );
+
+            if ($value === '') {
+
+                continue;
+            }
+
+            $name =
+                $labels[$class]
+                ?? $class;
 
             $results[$name] = $value;
         }
